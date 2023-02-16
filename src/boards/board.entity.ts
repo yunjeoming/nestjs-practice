d34a110fa6fa5200531db701808c83bfd93e1d33
yunjeoming <yunjeoming@gmail.com>
2023-02-16 21:14:28 +0900

    Add board entity and repository

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,17 @@
+import { BoardStatus } from './board.model';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { BoardRepository } from './board.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
