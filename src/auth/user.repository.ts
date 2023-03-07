1487db3614668e0f48d6cebe0e203cde81b8f22c
yunjeoming <yunjeoming@gmail.com>
2023-03-07 22:08:25 +0900

    Add sign up user

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,26 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  // db에 user의 정보를 저장해준다.
+  async createUser(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
